bfa6dbcb3b04ebb4f962bf5ce727572beffdd65d
HariSeldon51 <dehavenbooks@gmail.com>
2014-11-08 20:36:43 -0800

    Added Game States.

---
+++ src/com/dehavenmedia/brave_new_galaxy/GameState.java
@@ -0,0 +1,31 @@
+package com.dehavenmedia.brave_new_galaxy;
+
+public enum GameState {
+	ST_COMPANY {
+		// State's logic goes here
+	}, 
+	ST_TITLE {
+		// State's logic goes here
+	}, 
+	ST_INTRO {
+		// State's logic goes here
+	}, 
+	ST_MAINMENU {
+		// State's logic goes here
+	}, 
+	ST_SETUP {
+		// State's logic goes here
+	}, 
+	ST_LOAD {
+		// State's logic goes here
+	}, 
+	ST_GAMEWORLD {
+		// State's logic goes here
+	}, 
+	ST_EXIT {
+		// State's logic goes here
+	}, 
+	ST_NULL {
+		// State's logic goes here
+	};
+}
