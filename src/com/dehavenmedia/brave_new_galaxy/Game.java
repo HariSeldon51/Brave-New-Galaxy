6d5f363da305ab416d1f99d36352167c389142ae
HariSeldon51 <dehavenbooks@gmail.com>
2014-11-09 11:11:35 -0800

    Worked on adding in OpenGL and a opening image.

--- src/com/dehavenmedia/brave_new_galaxy/Game.java
+++ src/com/dehavenmedia/brave_new_galaxy/Game.java
@@ -3,6 +3,7 @@ package com.dehavenmedia.brave_new_galaxy;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 import com.dehavenmedia.brave_new_galaxy.Enums.GameState;
 
@@ -20,14 +21,7 @@ public class Game {
 	
 	//start method
 	public void start() {
-		try {
-			Display.setDisplayMode(new DisplayMode(800,600));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}		
-		// init OpenGL here
+		initGL(800, 600);
 	}
 	
 	//update method
@@ -45,5 +39,30 @@ public class Game {
 	public void end() {
 		Display.destroy();
 		System.exit(1);
+	}
+	
+	private void initGL(int width, int height) {
+		try {
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}		
+		GL11.glEnable(GL11.GL_TEXTURE_2D);               
+        
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);          
+        
+        // enable alpha blending
+       	GL11.glEnable(GL11.GL_BLEND);
+       	GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        
+       	GL11.glViewport(0,0,width,height);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, width, height, 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 	}
 }
